c73ae4ec426d35af12a71d21e82dd2f1bc833842
Cam  Henderson <camhenderson@Cams-MacBook-Air.local>
2022-09-12 12:16:13 -0700

    edits

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use yew::{function_component, html, use_state, Callback};
 
 #[function_component(UseState)]
 fn state() -> Html {
+
     let counter = use_state(|| 0);
+
     let onclick = {
         let counter = counter.clone();
         Callback::from(move |_| counter.set(*counter + 1))
     };
-
 
     html! {
         <div>
