ff398827c34250036000e888c4109eec3c0409f5
Cam  Henderson <camhenderson@Cams-MacBook-Air.local>
2022-09-12 11:46:53 -0700

    adding files for githubpages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use yew::{Callback, function_component, html, use_state};
+
+
+#[function_component(UseState)]
+fn state() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        Callback::from(move |_| counter.set(*counter + 1))
+    };
+
+
+    html! {
+        <div>
+            <p>
+                <b {onclick}>{ "Current value: " }{*counter}</b>
+            </p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<UseState>();
 }
